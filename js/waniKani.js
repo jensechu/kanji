e384be6e0cd3617147650a1c444b1c596d2a5ddb
Jensen <Jensechu@gmail.com>
2014-09-01 21:25:41 -0700

    :strawberry: Integrated WaniKani API. Load account kanji.

---
+++ js/waniKani.js
@@ -0,0 +1,50 @@
+window.WaniKani = {
+  handleLoadUserSubmit: function() {
+    var $waniKaniSubmit = $('.submitWaniKaniUser');
+
+    $waniKaniSubmit.on('click', function() {
+      var apiKey = $('.inputWaniKaniUser').val().trim();
+      WaniKani.loadUser(apiKey);
+    });
+  },
+
+  setUserKanji: function(userKanji) {
+    var $selectedCategory = $('.categoryBox[data-category="wanikani"] h2');
+    $.each(userKanji, function(i, kanji) {
+      kanji['category'] = 'wanikani';
+
+      window.Kanji._setKanjiSelector(kanji);
+    });
+
+    window.Kanji.sectionExpansion($selectedCategory);
+  },
+
+  hideUserForm: function() {
+    var $waniKaniForm = $('.waniKaniContent');
+
+    $waniKaniForm.hide();
+  },
+
+  loadUser: function(apiKey) {
+    $.ajax({
+      url: 'https://www.wanikani.com/api/user/'+ apiKey +'/kanji/1',
+      type: 'GET',
+      dataType: 'jsonp',
+
+      error: function(xhr, status, error) {
+          alert("error");
+      },
+
+      success: function(resp, status) {
+        var userKanji = resp.requested_information;
+
+        WaniKani.setUserKanji(userKanji);
+        WaniKani.hideUserForm();
+      }
+    });
+  },
+
+  init: function() {
+    WaniKani.handleLoadUserSubmit();
+  }
+}
