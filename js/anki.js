4b27ab0550ee72ccfee8c23fb34a9a7306dac592
Michael Goldbach <michael@m-goldbach.net>
2014-09-11 23:57:57 +0200

    added deck choice confirmation, added listing of models used for cards in chosen decks and radio list of model fields to be chosen for kanji scan.

--- js/anki.js
+++ js/anki.js
@@ -1,13 +1,15 @@
 window.Anki = {
-  db : [],
+  db : {},
+  decks : {},
+  models : {},
   deckTree : {},
   chosenDecks : {},
+  associatedModels : [],
+
 
   handleLoadCollection : function() {
     var $ankiSubmit = $('.submitAnkiCollection');
-    console.log("test anki handle");
     $ankiSubmit.on('click', function() {
-      console.log("click anki submit button");
        var file = $('.inputAnkiCollection')[0].files[0];
       if(file !== null){
         Anki.loadDbFile(file);
@@ -26,6 +28,7 @@ window.Anki = {
         console.log("loaded db file");
         var UInts = new Uint8Array(fileReader.result);
         Anki.readDb(UInts);
+        console.log("collection loaded")
       };
       fileReader.readAsArrayBuffer(file);
     }else {
@@ -36,31 +39,32 @@ window.Anki = {
   readDb : function(UInts) {
     var sql = window.SQL;
     var db = new sql.Database(UInts);
-    Anki.db[0] = db;
-    console.log(db);
+    if(db === undefined){
+      console.log("error loading file"); //Todo add some user notification
+      return undefined;
+    }
+    console.log("loaded file as SqLite database");
+    Anki.db = db;
+    Anki._parseCollectionConfig();
+  },
+
+  _parseCollectionConfig : function() {
+    //query existing decks in collection
     var deckQuery = "SELECT decks FROM col";
-    var result = db.exec(deckQuery);
+    var result = Anki.db.exec(deckQuery);
     console.log(result);
     var decks = JSON.parse(result[0].values[0][0]);
-
+    Anki.decks = decks;
+    //query existing card models
     var modelQuery = "SELECT models FROM col";
-    result = db.exec(modelQuery);
-    var models = JSON.parse(result[0].values[0][0]);
-    console.log(models);
-    Anki._parseModels(models);
-
-
-    console.log(decks);
-    Anki._parseDecks(decks);
-    //console.log(Anki._parseFields(result[0].values[0][0]))
+    result = Anki.db.exec(modelQuery);
+    Anki.models = JSON.parse(result[0].values[0][0]);
+    console.log(Anki.models);
+    Anki._parseDecks();
   },
 
-  _parseModels : function(models) {
-
-  },
-
-  _parseDecks : function(decks) {
-    $.each(decks, function(id, deck){
+  _parseDecks : function() {
+    $.each(Anki.decks, function(id, deck){
       var deckBranch = deck.name.split("::");
       var i;
       if(Anki.deckTree[deckBranch[0]] === undefined){
@@ -92,7 +96,11 @@ window.Anki = {
   //display root decks of collection
   _displayDecks : function(){
     var $ankiContent = $('.ankiContent');
-    var $listRoot = $('<ul>');
+    $ankiContent.empty();
+    $deckHeader = $("<h3>");
+    $deckHeader.text("Select Deck(s)");
+    $ankiContent.append($deckHeader);
+    var $listRoot = $('<ul>').addClass('ankiDeckList');
     $.each(Anki.deckTree, function(deckName, deck){
       var $deckCheckBox = $('<input>',{ 'type' : 'checkbox', 'id' : 'cb-'+deck.data.id, 'name' : deck.data.id });
       $deckCheckBox.addClass('ankiDeckCheckbox').addClass('root-did'+deck.data.id);
@@ -108,12 +116,19 @@ window.Anki = {
     });
 
     $ankiContent.append($listRoot);
+    var $submitDeckListButton = $('<button>').addClass('submitAnkiDeck');
+    $submitDeckListButton.text('next Step');
+    $ankiContent.append($submitDeckListButton);
+    $submitDeckListButton.on('click', function(e){
+      $ankiContent.empty();
+      Anki._loadAssociatedCardModels($ankiContent);
+    });
 
   },
 
   //display subdecks of other decks, subdecks (this is a recursive function!)
   _addSubdecks : function($deckListElement, currentDeck, rootDecks){
-    var $listRoot = $('<ul>');
+    var $listRoot = $('<ul>').addClass('subDeckList');
 
     $.each(currentDeck.subDecks, function(deckName, deck){
       var $deckCheckBox = $('<input>',{ 'type' : 'checkbox', 'id' : 'cb-'+deck.data.id, 'name' : deck.data.id });
@@ -150,19 +165,62 @@ window.Anki = {
       } else {
         Anki.chosenDecks[$thisBox.attr('name')] = false;
       }
-      Anki._getKanjis();
     });
 
   },
 
+  _loadAssociatedCardModels : function($ankiContent){
+    var modelQuery = "SELECT notes.mid " +
+      "FROM notes, cards " +
+      "WHERE cards.nid = notes.id " +
+      "  AND cards.did in " + Anki._getSelectedDecks()+
+      " group by notes.mid";
+    console.log(modelQuery);
+    var result = Anki.db.exec(modelQuery);
+    console.log(result);
+    var rawModelList = result[0].values;
+
+    var i;
+
+    var $listRoot = $('<ul>').addClass('ankiDeckList');
+
+    for(i= 0; i<rawModelList.length; i++){
+      var modelId = result[0].values[i][0];
+
+      var $rootElement = $("<li>");
+      $listRoot.append($rootElement);
+      var $listRootLabel = $('<label>');
+      $listRootLabel.text(Anki.models[modelId].name);
+      $rootElement.append($listRootLabel);
+
+      var $modelListRoot = $('<ul>').addClass('subDeckList');
+      $rootElement.append($modelListRoot);
+
+      Anki.associatedModels.push(modelId);
+
+      var j = 0;
+      for(j=0; j<Anki.models[modelId].flds.length; j++){
+        var $modelRadioItem = $('<input>',{ 'type' : 'radio', 'id' : 'aR-'+modelId+"-"+j, 'name' : modelId });
+        $modelRadioItem.addClass('ankiModelRadio');
+        var $modelListElement = $('<li>');
+        var $modelListLabel = $('<label>').attr('for', 'aR-'+modelId+"-"+j);
+        $modelListLabel.text(Anki.models[modelId].flds[j].name);
+        $modelListElement.append($modelRadioItem);
+        $modelListElement.append($modelListLabel);
+        $modelListRoot.append($modelListElement);
+      }
+    }
+    $ankiContent.append($listRoot);
+
+  },
+
   _getKanjis : function(){
-    var db =  Anki.db[0];
     var fldsQuery = "SELECT notes.flds FROM notes, cards " +
       "WHERE cards.nid=notes.id " +
       "   AND cards.did in " + Anki._getSelectedDecks() +
       "group by notes.id";
     console.log(fldsQuery);
-    var result = db.exec(fldsQuery);
+    var result = Anki.db.exec(fldsQuery);
     console.log(result);
     if(result[0] != undefined) {
       console.log(Anki._parseFields(result[0].values[0][0])[0]);
